531aea962efa4724ffb3d35d09ff2b7f3c34c251
Jay Allen <jay@endevver.com>
2014-11-08 15:16:00 -0800

    Simplify forwards

--- lib/MT/ConvertDB/ConfigMgr.pm
+++ lib/MT/ConvertDB/ConfigMgr.pm
@@ -43,6 +43,23 @@ sub BUILD {
     $self;
 }
 
+{
+    no warnings 'once';
+    *olddb = \&use_old_database;
+    *newdb = \&use_new_database;
+}
+sub object_summary  { shift->new_config->object_summary() }
+sub post_load       { shift->newdb->post_load(@_)         }
+sub post_load_meta  { shift->newdb->post_load_meta(@_)    }
+
+sub use_old_database {
+    ###l4p $l4p ||= get_logger(); $l4p->debug('Switching to old database');
+    $_[0]->old_config->use();
+}
+
+sub use_new_database {
+    ###l4p $l4p ||= get_logger(); $l4p->debug('Switching to new database');
+    $_[0]->new_config->use();
 }
 
 sub check_configs {
@@ -61,46 +78,6 @@ sub check_configs {
         if $orig->driver->{dsn} eq $new->driver->{dsn};
 
     $l4p->debug('@MT::ObjectDriverFactory::drivers: '.p(@MT::ObjectDriverFactory::drivers));
-}
-
-{
-    no warnings 'once';
-    *olddb = \&use_old_database;
-    *newdb = \&use_new_database;
-}
-
-sub use_old_database {
-    my $self = shift;
-    ###l4p $l4p ||= get_logger();
-    ###l4p $l4p->debug('Switching to old database');
-    $self->old_config->use();
-}
-
-sub use_new_database {
-    my $self = shift;
-    ###l4p $l4p ||= get_logger();
-    ###l4p $l4p->debug('Switching to new database');
-    $self->new_config->use();
-}
-
-sub object_summary {
-    my $self = shift;
-    ###l4p $l4p ||= get_logger(); $l4p->debug("Forwarding to new_config->object_summary");
-    $self->new_config->object_summary();
-}
-
-sub post_load {
-    my $self     = shift;
-    my $classobj = shift;   #  Can be classmgr
-    ###l4p $l4p ||= get_logger(); $l4p->debug("Forwarding to newdb->post_load");
-    $self->newdb->post_load( $classobj );
-}
-
-sub post_load_meta {
-    my $self     = shift;
-    my $classobj = shift;   #  Can be classmgr
-    ###l4p $l4p ||= get_logger(); $l4p->debug("Forwarding to newdb->post_load");
-    $self->newdb->post_load( $classobj );
 }
 
 1;
