47bbec66a5c03c19d281fe2db785889da39305ed
Jay Allen <jay@endevver.com>
2014-12-11 04:40:44 -0800

    POD documentation

--- README.md
+++ README.md
@@ -1,25 +1,219 @@
-mt-util-convertdb
-=================
+# NAME
 
-Coming soon...
+convertdb
 
-Requirements:
+# DESCRIPTION
 
-   * Log4MT plugin
-   * Class::Load
-   * Data::Printer
-   * Import::Base
-   * List::MoreUtils
-   * List::Util
-   * Module::Runtime
-   * Moo
-   * MooX::Options
-   * Path::Tiny
-   * Scalar::Util
-   * Sub::Quote
-   * Term::ProgressBar
-   * Test::Deep
-   * Text::Table
-   * ToolSet
-   * Try::Tiny
+This utility makes it possible to migrate Movable Type data between databases, regardless of
+database type. For example, you could use it to backup your MT data from one MySQL database to
+another or you could migrate your data to a completely different database (e.g. Oracle to MySQL).
 
+# SYNOPSIS
+
+convertdb \[-h\] \[long options ...\]
+
+    cd $MT_HOME
+    CONVERTDB="plugins/ConvertDB/tools/convertdb --new mt-config-NEW.cgi"
+
+    # Need help??
+    $CONVERTDB --usage                              # Show compact usage syntax
+    $CONVERTDB --help                               # Show help text
+    $CONVERTDB --man                                # Show man page
+
+    # Migration modes
+    $CONVERTDB --mode resavesource                  # Prep source DB
+    $CONVERTDB --mode migrate                       # Migrate and verify
+
+    # Inspection/verification modes
+    $CONVERTDB --mode verify                        # Reverify data
+    $CONVERTDB --mode showcounts                    # Compare table counts
+    $CONVERTDB --mode checkmeta                     # Check for orphaned/unregistered
+
+    # Metadata cleanup
+    $CONVERTDB --mode checkmeta --remove-orphans    # Remove the orphaned
+    $CONVERTDB --mode checkmeta --migrate-unknown   # Migrate the unregistered
+
+# OPTIONS
+
+- **--mode: String**
+
+    \[REQUIRED\] Run modes. See the ["MODES"](#modes) section for the list of valid values.
+
+- **--new\_config: String**
+
+    \[REQUIRED\] Use this option to specify the path/filename of the MT config file containing the new database information.  It can be an absolute path or relative to MT\_HOME (e.g. ./mt-config-new.cgi)
+
+- **--old\_config: String**
+
+    Use this to specify the path/filename of the current MT config file.  It defaults to ./mt-config-cgi so you only need to use it if you want to set specific configuration directives which are different than the ones in use in the MT installation.
+
+- **--classes: Array of Strings**
+
+    Use this to specify one or more classes you want to act on in the specified
+    mode. The value can be comma-delimited or you can specify this option multiple
+    times for multiple classes. For example, the following are equivalent:
+
+        --classes MT::Blog --classes MT::Author --classes MT::Entry
+        --classes MT::Blog,MT::Author,MT::Entry
+
+    This is useful if you want to execute a particular mode on a single table or a small handful of tables. For example:
+
+        --mode migrate --class MT::Blog
+        --mode showcounts --class MT::Blog
+        --mode checkmeta --classes MT::Blog,MT::Author,MT::Entry
+
+- **--skip\_classes: Array of Strings**
+
+    Use this to specify one or more classes to exclude during execution of the specified mode. This
+    is the exact inverse of `--classes`, has the same syntax and is most useful for excluding one
+    or a small handful of classes.
+
+        --skip-classes MT::Log,MT::Log::Entry,MT::Log::Comment[,...]
+        
+
+- **--skip\_tables: Array of Strings**
+
+    Use this to specify one or more tables to exclude during execution of the specified mode. This
+    is similar to `--skip_classes` but often shorter and more likely what you want. For example,
+    the following skips the entire mt\_log table and is equivalent to the example above. Note that
+    you omit the `mt_` prefix of the table:
+
+        --skip-table log
+
+    It is recommended that you always use this option (especially with with **migrate** or **verify**
+    modes) unless you need to preserve your Activity log data:
+
+        --mode migrate --skip-table log
+        --mode verify --skip-table log
+        --mode showcounts --skip-table log
+        
+
+- **--no\_verify:**
+
+    Under **migrate** mode, this option skips the content and encoding verification for each object
+    migrated to the source database. This is useful if you want to quickly perform a migration and
+    are confident of the process or plan on verifying later.
+
+- **--migrate\_unknown:**
+
+    Under **checkmeta** mode, this option cause all metadata records with unregistered field types to be migrated.  This normally doesn't happen under **migrate** mode which only transfers object metadata with registered field types.'
+
+- **--remove\_orphans:**
+
+    Under **checkmeta** mode, this removes all metadata records from the source database which are associated with a non-existent object.
+
+- **--dry\_run:**
+
+    A debugging option which marks the target database as read-only
+
+- **--usage:**
+
+    show a short help message
+
+- **-h --help:**
+
+    show a help message
+
+- **--man:**
+
+    show the manual
+
+# MODES
+
+convertdb's run mode is specified using the `--mode` option (see ["OPTIONS"](#options)).
+All values are case-insensitive and the word separator can be a hyphen, an
+underscore or omitted entirely (checkmeta, check-meta, check\_meta, CheckMeta,
+etc)
+
+All of the modes described below iterate over a master list of all object
+classes (and their respective metadata classes and tables) in use by Movable
+Type. You can modify this list using `--classes`, `--skip-classes` and
+`--skip-tables`. This is extremely useful for acting on all but a few large
+tables or performing a mode only on a single or handful of classes/tables.
+
+## Supported Modes
+
+The following is a list of all supported values for the `--mode` flag:
+
+- **resave\_source**
+
+    Iteratively load each object of the specified class(es) from the source database
+    (in all/included classes minus excluded) and then resave them back to the
+    source database. Doing this cleans up all of the metadata records which have
+    null values and throw off the counts.
+
+    This is one of only two modes which affect the source database (the other is
+    `--mode checkmeta --remove-orphans`) and it only needs to be run once. It is
+    most efficient to execute this mode first in order to clean up the table counts
+    for later verification.
+
+- **check\_meta**
+
+    Perform extra verification steps on metadata tables for the specified class(es)
+    looking for orphaned and unused metadata rows. With other flags, you can remove
+    or even migrate the found rows. It is highly recommended to run this with
+    `--remove-orphans` and `--migrate-unknown` flags described later.
+
+- **migrate**
+
+    Iteratively loads each object of the specified class(es) and its associated
+    metadata from the source database and saves all of it to the target database.
+
+    By default, the utility also performs an additional step in verifying that the
+    data loaded from the source and re-loaded from the target is exactly the same,
+    both in content and encoding. If you wish to skip this verification step you
+    can use the `--no-verify` flag.
+
+    We highly recommend you use `--skip-table=log` unless you need to preserve the
+    activity log history because it can easily dwarf the actual user-created
+    content in the database.
+
+- **verify**
+
+    Performs the same verification normally performed by default under migrate
+    mode, only without the data migration. This is the exact opposite of `--mode
+    migrate --no-verify`.
+
+- **show\_counts**
+
+    Shows the object and metadata table counts for the specified class(es) in both the current and new databaseq.
+
+# INSTALLATION
+
+You can download an archived version of this utility from
+\[Github/endevver/mt-util-convertdb\](https://github.com/endevver/mt-util-convertdb) or use git:
+
+        $ cd $MT_HOME;
+        $ git clone git@github.com:endevver/mt-util-convertdb.git plugins/ConvertDB
+
+Due to a silly little quirk in Movable Type, the utility must be installed as
+\`plugins/ConvertDB\` and not \`plugins/mt-util-convertdb\` as is the default.
+
+## DEPENDENCIES
+
+- Movable Type 5 or higher
+- Log4MT plugin
+- Class::Load
+- Data::Printer
+- Import::Base
+- List::MoreUtils
+- List::Util
+- Module::Runtime
+- Moo
+- MooX::Options
+- Path::Tiny
+- Scalar::Util
+- Sub::Quote
+- Term::ProgressBar
+- Test::Deep
+- Text::Table
+- ToolSet
+- Try::Tiny
+- Term::Prompt
+- Pod::POM
+- SQL::Abstract
+- Path::Class
+
+# AUTHOR
+
+Jay Allen, Endevver LLC <jay@endevver.com>
--- lib/MT/ConvertDB/CLI.pm
+++ lib/MT/ConvertDB/CLI.pm
@@ -4,9 +4,24 @@ use MT::ConvertDB::ToolSet;
 use Term::ProgressBar 2.00;
 use List::Util qw( reduce );
 use List::MoreUtils qw( none part );
-use MooX::Options;
 use Term::Prompt qw( prompt );
 use Sub::Quote qw( quote_sub );
+use Pod::Usage qw( pod2usage );
+
+use Pod::POM;
+my ( @POD_EXTRACT, %MooXOptions, $parser, $pom );
+BEGIN {
+    @POD_EXTRACT = qw( name synopsis description );
+    $parser      = Pod::POM->new;
+    $pom         = $parser->parse(__FILE__) or die $parser->error();
+    for my $head1 ( $pom->head1 ) {
+        my $label = lc($head1->title);
+        next unless grep { $label eq $_ } @POD_EXTRACT;
+        $MooXOptions{$label} = $head1->content;
+    }
+    # $MooXOptions{flavour} = [qw( pass_through )];
+}
+use MooX::Options ( %MooXOptions );
 use vars qw( $l4p );
 
 has mode_handlers => (
@@ -16,38 +31,41 @@ has mode_handlers => (
             showcounts   => 'do_table_counts',
             checkmeta    => 'do_check_meta',
             resavesource => 'do_resave_source',
-            migrate       => 'do_migrate_verify',
-            verify        => 'do_migrate_verify',
-            test          => 'do_test',
+            migrate      => 'do_migrate_verify',
+            verify       => 'do_migrate_verify',
+            test         => 'do_test',
             fullmigrate  => 'do_full_migrate_verify'
         }
     },
 );
 
 option mode => (
-    is      => 'ro',
-    format  => 's',
-    doc     => 'REQUIRED run mode: show-counts, resave-source, check-meta, migrate, verify, test. Modes are case-insensitive and the word separator can be a hyphen, an underscore or omitted entirely (checkmeta, check-meta, check_meta, CheckMeta, etc)',
-    longdoc => '',
-    coerce  => quote_sub(q( ($_[0] = lc($_[0])) =~ s/[^a-z]//g; $_[0]  )),
-    default => 'init_only',
+    is       => 'ro',
+    format   => 's',
+    doc      => '[REQUIRED] Run mode: show_counts, resave_source, check_meta, migrate or verify.',
+    long_doc => q([REQUIRED] Run modes. See the L</MODES> section for the list of valid values.),
+    coerce   => quote_sub(q( ($_[0] = lc($_[0])) =~ s/[^a-z]//g; $_[0]  )),
+    default  => 'init_only',
     required => 1,
-);
-
-option old_config => (
-    is      => 'ro',
-    format  => 's',
-    doc     => 'Path to config file for current database. Can be relative to MT_HOME',
-    longdoc => '',
-    default => './mt-config.cgi',
+    order    => 1,
 );
 
 option new_config => (
     is       => 'ro',
     format   => 's',
     required => 1,
-    doc      => 'REQUIRED: Path to config file for new database. Can be relative to MT_HOME',
-    longdoc  => '',
+    doc      => '[REQUIRED] Path to config file for new database. Can be relative to MT_HOME',
+    long_doc => q([REQUIRED] Use this option to specify the path/filename of the MT config file containing the new database information.  It can be an absolute path or relative to MT_HOME (e.g. ./mt-config-new.cgi)),
+    order    => 2,
+);
+
+option old_config => (
+    is       => 'ro',
+    format   => 's',
+    doc      => 'Path to current config file. Can be relative to MT_HOME. Defaults to ./mt-config.cgi',
+    long_doc => q(Use this to specify the path/filename of the current MT config file.  It defaults to ./mt-config-cgi so you only need to use it if you want to set specific configuration directives which are different than the ones in use in the MT installation.),
+    default  => './mt-config.cgi',
+    order    => 3,
 );
 
 option classes => (
@@ -55,8 +73,21 @@ option classes => (
     format    => 's@',
     autosplit => ',',
     default   => sub { [] },
-    doc       => 'Classes to include. Can be comma-delimited or specified multiple times',
-    longdoc   => '',
+    doc       => 'Classes to include (e.g. MT::Blog). Can be comma-delimited or specified multiple times',
+    long_doc  => q(
+Use this to specify one or more classes you want to act on in the specified
+mode. The value can be comma-delimited or you can specify this option multiple
+times for multiple classes. For example, the following are equivalent:
+
+    --classes MT::Blog --classes MT::Author --classes MT::Entry
+    --classes MT::Blog,MT::Author,MT::Entry
+
+This is useful if you want to execute a particular mode on a single table or a small handful of tables. For example:
+
+    --mode migrate --class MT::Blog
+    --mode showcounts --class MT::Blog
+    --mode checkmeta --classes MT::Blog,MT::Author,MT::Entry),
+    order     => 4,
 );
 
 option skip_classes => (
@@ -65,7 +96,13 @@ option skip_classes => (
     autosplit => ',',
     default   => sub { [] },
     doc       => 'Classes to skip (e.g. MT::Log). Can be comma-delimited or specified multiple times',
-    longdoc   => '',
+    long_doc  => q(Use this to specify one or more classes to exclude during execution of the specified mode. This
+is the exact inverse of C<--classes>, has the same syntax and is most useful for excluding one
+or a small handful of classes.
+
+    --skip-classes MT::Log,MT::Log::Entry,MT::Log::Comment[,...]
+    ),
+    order     => 5,
 );
 
 option skip_tables => (
@@ -73,55 +110,69 @@ option skip_tables => (
     format    => 's@',
     autosplit => ',',
     default   => sub { [] },
-    doc       => 'Tables to skip (without the "mt_" prefix, e.g. log ). Can be comma-delimited or specified multiple times',
-    longdoc   => '',
-);
+    doc       => 'Tables to skip (omit "mt_" prefix: log). Can be comma-delimited or specified multiple times',
+    long_doc  => q(Use this to specify one or more tables to exclude during execution of the specified mode. This
+is similar to C<--skip_classes> but often shorter and more likely what you want. For example,
+the following skips the entire mt_log table and is equivalent to the example above. Note that
+you omit the C<mt_> prefix of the table:
 
-option dry_run => (
-    is      => 'rw',
-    doc     => 'DEBUG: Marks new database as read-only for testing during migrate mode',
-    longdoc => '',
-    default => 0,
+    --skip-table log
+
+It is recommended that you always use this option (especially with with B<migrate> or B<verify>
+modes) unless you need to preserve your Activity log data:
+
+    --mode migrate --skip-table log
+    --mode verify --skip-table log
+    --mode showcounts --skip-table log
+    ),
+    order     => 6,
 );
 
 option no_verify => (
-    is      => 'ro',
-    doc     => 'Skip verification of data. Only valid with migrate mode',
-    longdoc => '',
-    default => 0,
+    is       => 'ro',
+    doc      => '[WITH MODE: migrate] Skip data verification during migration.',
+    long_doc => q(Under B<migrate> mode, this option skips the content and encoding verification for each object
+migrated to the source database. This is useful if you want to quickly perform a migration and
+are confident of the process or plan on verifying later.),
+
+    default  => 0,
+    order    => 7,
 );
 
 option migrate_unknown => (
-    is      => 'ro',
-    doc     => 'Migrate all unknown metadata. Only valid under check_meta mode',
-    longdoc => '',
-    default => 0,
+    is       => 'ro',
+    doc      => '[WITH MODE: checkmeta] Migrate all unknown metadata.',
+    long_doc => q(Under B<checkmeta> mode, this option cause all metadata records with unregistered field types to be migrated.  This normally doesn't happen under B<migrate> mode which only transfers object metadata with registered field types.'),
+    default  => 0,
+    order    => 8,
 );
 
 option remove_orphans => (
+    is       => 'ro',
+    doc      => '[WITH MODE: checkmeta] Remove found orphans.',
+    long_doc => q(Under B<checkmeta> mode, this removes all metadata records from the source database which are associated with a non-existent object.),
+    default  => 0,
+    order    => 9,
+);
+
+option remove_obsolete => (
     is      => 'ro',
-    doc     => 'Remove found orphans. Only valid under check_meta mode',
-    longdoc => '',
+    doc     => 'hidden',
     default => 0,
 );
 
-# option remove_obsolete => (
-#     is      => 'ro',
-#     doc     => '[WITH MODE: checkmeta] Remove obsolete metadata as defined by the RetiredFields plugin. You probably should not use this.',
-#     long_doc => q(),
-#     default => 0,
-#     order   => 10,
-# );
-#
-# option remove_unused => (
-#     is      => 'ro',
-#     doc     => '[WITH MODE: checkmeta] Remove all unused (i.e. unregistered) metadata. You probably should not use this.',
-#     long_doc => q(),
-#     default => 0,
-#     order   => 11,
-# );
-
+option remove_unused => (
+    is      => 'ro',
+    doc     => 'hidden',
     default => 0,
+);
+
+option dry_run => (
+    is       => 'rw',
+    doc      => 'DEBUG: Marks new database as read-only for testing during migrate mode',
+    long_doc => q(A debugging option which marks the target database as read-only),
+    default  => 0,
+    order    => 12,
 );
 
 has classmgr => ( is => 'lazy', );
@@ -347,49 +398,6 @@ sub do_test {
     my $classmgr   = $self->classmgr;
     my $class_objs = $self->class_objects;
     ###l4p $l4p ||= get_logger();
-
-=pod
-
-    use DBIx::Compare;
-
-    say "Instantiation....";
-    my $oDB_Comparison = db_comparison->new(
-        $cfgmgr->olddb->driver->dbh,
-        $cfgmgr->newdb->driver->dbh,
-    );
-
-    say "Verbose...";
-    $oDB_Comparison->verbose;
-
-    say "Comparing.";
-    $oDB_Comparison->compare;
-
-    say "Deep comparing.";
-    $oDB_Comparison->deep_compare;
-
-    # $oDB_Comparison->deep_compare(@aTable_Names);
-    exit;
-    p( MT::Meta->metadata_by_class('MT::Website') );
-    p( MT::Meta->has_own_metadata_of('MT::Website') );
-    p( MT::Website->meta_pkg->properties );
-    my @sites = MT->model('blog:meta')->load();
-    p(@sites);
-    exit;
-
-    # foreach my $c ( @$class_objs ) {
-    #
-    # }
-
-   my @entries = MT::Entry->load(undef, {
-        'join' => MT::Comment->join_on( 'entry_id',
-                    { blog_id => $blog_id },
-                    { 'sort' => 'created_on',
-                      direction => 'descend',
-                      unique => 1,
-                      limit => 10 } )
-    });
-=cut
-
 }
 
 sub do_resave_source {
@@ -717,3 +725,166 @@ sub progress {
 
 1;
 
+__END__
+
+=head1 NAME
+
+convertdb
+
+=head1 DESCRIPTION
+
+This utility makes it possible to migrate Movable Type data between databases, regardless of
+database type. For example, you could use it to backup your MT data from one MySQL database to
+another or you could migrate your data to a completely different database (e.g. Oracle to MySQL).
+
+=head1 SYNOPSIS
+
+    cd $MT_HOME
+    CONVERTDB="plugins/ConvertDB/tools/convertdb --new mt-config-NEW.cgi"
+
+    # Need help??
+    $CONVERTDB --usage                              # Show compact usage syntax
+    $CONVERTDB --help                               # Show help text
+    $CONVERTDB --man                                # Show man page
+
+    # Migration modes
+    $CONVERTDB --mode resavesource                  # Prep source DB
+    $CONVERTDB --mode migrate                       # Migrate and verify
+
+    # Inspection/verification modes
+    $CONVERTDB --mode verify                        # Reverify data
+    $CONVERTDB --mode showcounts                    # Compare table counts
+    $CONVERTDB --mode checkmeta                     # Check for orphaned/unregistered
+
+    # Metadata cleanup
+    $CONVERTDB --mode checkmeta --remove-orphans    # Remove the orphaned
+    $CONVERTDB --mode checkmeta --migrate-unknown   # Migrate the unregistered
+
+=head1 MODES
+
+convertdb's run mode is specified using the C<--mode> option (see L</OPTIONS>).
+All values are case-insensitive and the word separator can be a hyphen, an
+underscore or omitted entirely (checkmeta, check-meta, check_meta, CheckMeta,
+etc)
+
+All of the modes described below iterate over a master list of all object
+classes (and their respective metadata classes and tables) in use by Movable
+Type. You can modify this list using C<--classes>, C<--skip-classes> and
+C<--skip-tables>. This is extremely useful for acting on all but a few large
+tables or performing a mode only on a single or handful of classes/tables.
+
+=head2 Supported Modes
+
+The following is a list of all supported values for the C<--mode> flag:
+
+=over 4
+
+=item * B<resave_source>
+
+Iteratively load each object of the specified class(es) from the source database
+(in all/included classes minus excluded) and then resave them back to the
+source database. Doing this cleans up all of the metadata records which have
+null values and throw off the counts.
+
+This is one of only two modes which affect the source database (the other is
+C<--mode checkmeta --remove-orphans>) and it only needs to be run once. It is
+most efficient to execute this mode first in order to clean up the table counts
+for later verification.
+
+=item * B<check_meta>
+
+Perform extra verification steps on metadata tables for the specified class(es)
+looking for orphaned and unused metadata rows. With other flags, you can remove
+or even migrate the found rows. It is highly recommended to run this with
+C<--remove-orphans> and C<--migrate-unknown> flags described later.
+
+=item * B<migrate>
+
+Iteratively loads each object of the specified class(es) and its associated
+metadata from the source database and saves all of it to the target database.
+
+By default, the utility also performs an additional step in verifying that the
+data loaded from the source and re-loaded from the target is exactly the same,
+both in content and encoding. If you wish to skip this verification step you
+can use the C<--no-verify> flag.
+
+We highly recommend you use C<--skip-table=log> unless you need to preserve the
+activity log history because it can easily dwarf the actual user-created
+content in the database.
+
+=item * B<verify>
+
+Performs the same verification normally performed by default under migrate
+mode, only without the data migration. This is the exact opposite of C<--mode
+migrate --no-verify>.
+
+=item * B<show_counts>
+
+Shows the object and metadata table counts for the specified class(es) in both the current and new databaseq.
+
+=back
+
+=head1 INSTALLATION
+
+You can download an archived version of this utility from
+[Github/endevver/mt-util-convertdb](https://github.com/endevver/mt-util-convertdb) or use git:
+
+        $ cd $MT_HOME;
+        $ git clone git@github.com:endevver/mt-util-convertdb.git plugins/ConvertDB
+
+Due to a silly little quirk in Movable Type, the utility must be installed as
+`plugins/ConvertDB` and not `plugins/mt-util-convertdb` as is the default.
+
+=head2 DEPENDENCIES
+
+=over 4
+
+=item * Movable Type 5 or higher
+
+=item * Log4MT plugin
+
+=item * Class::Load
+
+=item * Data::Printer
+
+=item * Import::Base
+
+=item * List::MoreUtils
+
+=item * List::Util
+
+=item * Module::Runtime
+
+=item * Moo
+
+=item * MooX::Options
+
+=item * Path::Tiny
+
+=item * Scalar::Util
+
+=item * Sub::Quote
+
+=item * Term::ProgressBar
+
+=item * Test::Deep
+
+=item * Text::Table
+
+=item * ToolSet
+
+=item * Try::Tiny
+
+=item * Term::Prompt
+
+=item * Pod::POM
+
+=item * SQL::Abstract
+
+=item * Path::Class
+
+=head1 AUTHOR
+
+Jay Allen, Endevver LLC <jay@endevver.com>
+
+=cut
